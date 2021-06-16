d17bab540b00aabf0f216edb936933c139dca6a0
Thales Augusto <thallles.ramos@gmail.com>
2021-06-15 23:35:18 -0300

    adjustments in models

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,14 +1,13 @@
-class Contract < ApplicationRecord
+class Contract < ApplicationRecord  
   belongs_to :pilot
   belongs_to :payload, class_name: "Resource"
   belongs_to :origin, class_name: "Planet"
   belongs_to :destiny, class_name: "Planet"
 
-  enum state: %i[opened processed finished]
+  enum state: %i[opened processing finished]
 
-  validates_presence_of :resource_weight, :value, :state
+  validates_presence_of :payload_weight, :value, :state
   validates :description, length: { in: 0..5000 }
-  validates :resource_weight, numericality: { only_integer: { greater_than: 0 } }
+  validates :payload_weight, numericality: { only_integer: { greater_than: 0 } }
   validates :value, numericality: { only_integer: { greater_than: 0 } }
-  validates :state, inclusion: { in: states.keys }
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report < ApplicationRecord
-  belongs_to :reportable
+  belongs_to :reportable, polymorphic: true
 
   validates :description, presence: true, length: { in: 2..5000 }
 end
