bdfc418ea3c27319b5682cebcde59e50d23685e8
Thales Augusto <thallles.ramos@gmail.com>
2021-06-16 23:43:28 -0300

    fix validates with numbers

--- app/models/pilot.rb
+++ app/models/pilot.rb
@@ -4,8 +4,8 @@ class Pilot < ApplicationRecord
   validates_presence_of :certification, :name, :age, :credits
   validates :certification, length: { is: 7 }
   validates :name, length: { in: 2..255 }
-  validates :age, numericality: { only_integer: { greater_than: 0 } }
-  validates :credits, numericality: { only_integer: { greater_than_or_equal_to: 0 } }
+  validates :age, numericality: { greater_than: 0 }
+  validates :credits, numericality: { greater_than_or_equal_to: 0 }
   validate :check_luhn_certification
 
   def check_luhn_certification
