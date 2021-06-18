e057059fe7de1afc11ff5e0ab27905c273bed915
Thales Augusto <thallles.ramos@gmail.com>
2021-06-17 23:01:56 -0300

    creating state machine for contract

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,10 +1,10 @@
-class Contract < ApplicationRecord  
+class Contract < ApplicationRecord
+  include Stateable
+  
   belongs_to :pilot, optional: true
   belongs_to :payload, class_name: "Resource"
   belongs_to :origin, class_name: "Planet"
   belongs_to :destiny, class_name: "Planet"
-
-  enum state: { opened: 'opened', processing: 'processing', finished: 'finished' }, _default: :opened
 
   scope :opened, -> { where(state: 'opened') }
 
