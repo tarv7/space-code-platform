a8a7e507c34a4790a3e8c7bf90957900d68cbd57
Thales Augusto <thallles.ramos@gmail.com>
2021-06-17 23:04:33 -0300

    creating endpoint for accept transport contracts

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :pilots, only: :create
   resources :contracts, only: :create do
     get :opened, on: :collection, to: 'contracts/openeds#index'
+
+    patch :accept, on: :member, to: 'contracts/accepteds#update'
   end
 end
