dd3ca17da5485d0794128c53e79921faa54e6690
Thales Augusto <thallles.ramos@gmail.com>
2021-06-17 23:04:02 -0300

    creating "auth" via header params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::API
   rescue_from StandardError, with: :standard_error
 
+  protected
+
+  def current_pilot
+    @_current_pilot ||= Pilot.find(request.headers["auth-pilot-id"].to_i)
+  end
+
   private
 
   def standard_error(exception)
