2c77c96b1e53ba7558720493109d52d5cb70bc8d
Thales Augusto <thallles.ramos@gmail.com>
2021-06-18 01:11:25 -0300

    creating business class for update fuel of the ships

---
+++ app/business/refil_fuel.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class RefilFuel
+  class RefilFuelError < StandardError; end
+
+  UNIT_COST = 7
+  
+  def initialize(ship, fuel_quantity)
+    @ship = ship
+    @fuel_quantity = fuel_quantity
+  end
+
+  def call!
+    raise RefilFuelError, 'Amount of refill needs to be positive' unless fuel_quantity.positive?
+
+    Ship.transaction do
+      ship.pilot.update!(credits: new_credits)
+      ship.update!(fuel_level: new_fuel)
+    end
+  end
+
+  def self.call!(ship, fuel_quantity)
+    self.new(ship, fuel_quantity).call!
+  end
+
+  private
+
+  attr_accessor :ship, :fuel_quantity
+
+  def new_fuel
+    ship.fuel_level + fuel_quantity
+  end
+
+  def new_credits
+    decrease_credits = fuel_quantity * UNIT_COST
+
+    ship.pilot.credits - decrease_credits
+  end
+end
