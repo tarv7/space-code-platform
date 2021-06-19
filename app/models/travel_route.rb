a31c30128299a52003515fa3638c38dd98a0ff01
Thales Augusto <thallles.ramos@gmail.com>
2021-06-19 01:59:59 -0300

    clear cache after commit travel routes

--- app/models/travel_route.rb
+++ app/models/travel_route.rb
@@ -1,6 +1,14 @@
-class TravelRoute < ApplicationRecord
+class TravelRoute < ApplicationRecord  
   belongs_to :origin, class_name: "Planet"
   belongs_to :destiny, class_name: "Planet"
 
   validates :cost, presence: true, numericality: { greater_than: 0 }
+
+  after_save :clear_cache
+
+  private
+
+  def clear_cache
+    Rails.cache.delete(CalculateRoutes::KEY_CACHE_BEST_PATHS)
+  end
 end
