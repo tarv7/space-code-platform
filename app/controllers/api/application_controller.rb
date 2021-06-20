14c3ef3a4fb230fb4aedf2ba43ef4b2ff4856d45
Thales Augusto <thallles.ramos@gmail.com>
2021-06-19 22:57:29 -0300

    creating middleware for authentication

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,13 +1,21 @@
 module Api
   class ApplicationController < ::ApplicationController
+    class AuthenticateError < StandardError; end
+    
     rescue_from StandardError, with: :handle_standard_error
-
+  
     private
-
+  
+    def user_authenticate!
+      current_pilot.present?
+    rescue ActiveRecord::RecordNotFound
+      raise AuthenticateError, 'You need to log into the system'
+    end
+  
     def current_pilot
       @_current_pilot ||= Pilot.find(request.headers["auth-pilot-id"].to_i)
     end
-
+  
     def handle_standard_error(exception)
       render json: { message: exception.message }, status: :bad_request
     end
